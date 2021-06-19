ec1d8d9a38774bf99d30955f29b09d54692ad9a0
yanghoushan <yanghoushan@zdsoft.cn>
2021-06-19 22:09:43 +0800

    new

---
+++ js/banner.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#news-slider").owlCarousel({
+		items:5,
+		itemsDesktop:[1199,4],
+		itemsDesktopSmall:[980,3],
+		itemsMobile:[600,1],
+		pagination:false,
+		navigationText:false,
+		autoPlay:true
+	});
+});
